5ffe2884948e90f67214a72c92a50eaf1e288b0d
luqi <979651731@qq.com>
2018-10-10 19:54:48 +0800

    first commit

---
+++ ga-security-demo/src/test/java/com.ga.security.controller/UserControllerTest.java
@@ -0,0 +1,42 @@
+package com.ga.security.controller;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.reactive.function.server.MockServerRequest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+import javax.annotation.Resource;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UserControllerTest {
+
+    @Resource
+    private WebApplicationContext wac;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setup(){
+        mockMvc=MockMvcBuilders.webAppContextSetup(wac).build();
+    }
+
+    @Test
+    public void whenQuerySuccess() throws Exception {
+            mockMvc.perform(MockMvcRequestBuilders.get("/hello")
+                    .contentType(MediaType.APPLICATION_JSON_UTF8))
+                    .andExpect(MockMvcResultMatchers.status().isOk())
+                    .andExpect(MockMvcResultMatchers.jsonPath("$.length()").value(3));
+    }
+
+}
